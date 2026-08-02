47ccc8ef07d792fdc0835185c1879f9f8fa4e06b
Sebastien Collenne <collenne.seb@gmail.com>
2026-08-03 01:43:13 +0200

    fix(public-site): prevent mobile horizontal overflow

--- components/public/PublicSiteShell.tsx
+++ components/public/PublicSiteShell.tsx
@@ -10,7 +10,7 @@ export function PublicSiteShell({ children }: PublicSiteShellProps) {
   return (
     <div className="min-h-screen bg-[#020817] text-white">
       <PublicSiteHeader />
-      <main className="mx-auto w-[calc(100%-2.5rem)] max-w-[1640px] px-5 py-8 sm:w-[calc(100%-3rem)] sm:px-8 lg:w-[calc(100%-4rem)] lg:px-10 lg:py-12">
+      <main className="mx-auto w-full max-w-[1640px] px-5 py-8 sm:px-8 lg:px-10 lg:py-12">
         {children}
       </main>
       <PublicSiteFooter />
